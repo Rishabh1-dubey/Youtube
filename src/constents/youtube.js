4ed81cb00edeab48ffe93f17686c2ccef49501de
Rishabh Dubey <rishabhdubey006@gmail.com>
2025-01-22 02:21:16 +0530

    adding youtube api

---
+++ src/constents/youtube.js
@@ -0,0 +1,3 @@
+export const YOUTUBE_BASE_URL = "https://www.googleapis.com/youtube/v3"
+export const YOUTUBE_API="AIzaSyCoL_vVIuzheKRYmztT3orVQDBYwDvzRIQ"
+export const YOUTUBE_VIDEO_URL =` https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=20&regionCode=IN&key=${YOUTUBE_API}`
