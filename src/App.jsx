8ae69b467c305e54a8ae871048b91c7769241f96
Rishabh Dubey <rishabhdubey006@gmail.com>
2025-01-22 00:53:29 +0530

    adding store

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Navbar from './components/Navbar'
-import Sidebar from './components/Sidebar'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
+import Feed from "./components/Watchpage/Feed";
 
 function App() {
   return (
-     <div>
-     <Navbar/>
-     <Sidebar/>
-     </div>
-        
-    
-  )
+    <div className="">
+      <Navbar/>
+      <div className="flex bg-black">
+        <Sidebar />
+        <Feed />
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
