f7c7e071036392e8df522a3228805886271a133f
Rishabh Dubey <rishabhdubey006@gmail.com>
2025-01-22 13:11:43 +0530

    adding youtube_url

--- src/constents/youtube.js
+++ src/constents/youtube.js
@@ -1,3 +1,4 @@
 export const YOUTUBE_BASE_URL = "https://www.googleapis.com/youtube/v3"
 export const YOUTUBE_API="AIzaSyCoL_vVIuzheKRYmztT3orVQDBYwDvzRIQ"
 export const YOUTUBE_VIDEO_URL =` https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=20&regionCode=IN&key=${YOUTUBE_API}`
+
