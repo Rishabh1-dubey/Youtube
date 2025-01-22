8959a81eae9c0d8422289e2ea06782079130c7d6
Rishabh Dubey <rishabhdubey006@gmail.com>
2025-01-22 13:12:53 +0530

    adding router into our app

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
   return (
     <div className="">
       <Navbar/>
-      <div className="flex bg-black">
+      <div className="flex">
         <Sidebar />
         <Feed />
       </div>
--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,19 @@ import "./index.css";
 import App from "./App.jsx";
 import { Provider } from "react-redux";
 import { store } from "./utils/store.js";
+import WatchPage from "./components/Watchpage/WatchPage.jsx";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <App />
+      <BrowserRouter>
+    <Routes>
+      <Route path="" element={<App />} />
+      <Route path="/watch" element={<WatchPage />}>
+      <Route path="/watch/:id" element={<WatchPage/>}/>
+      </Route>
+    </Routes>
+  </BrowserRouter>
+
   </Provider>
 );
