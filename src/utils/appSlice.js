a7fc28d88aea4bc2a77fa4ecd7b5f4bc99dc7608
Rishabh Dubey <rishabhdubey006@gmail.com>
2025-01-24 21:55:57 +0530

    adding serachslice

--- src/utils/appSlice.js
+++ src/utils/appSlice.js
@@ -6,7 +6,8 @@ export const appSlice = createSlice({
   initialState:{
   open:true,
   video:[],
-  category:"All"
+  category:"All",
+  searchSuggestion:[]
   },
   reducers: {
   toggleSidebar:(state)=>{
@@ -17,10 +18,13 @@ export const appSlice = createSlice({
   },
   setCategory:(state,action)=>{
     state.category= action.payload
+  } ,
+  setSeachSuggestion:(state,action)=>{
+    state.searchSuggestion= action.payload
   }
 }
   })
 // Action creators are generated for each case reducer function
-export const { toggleSidebar,setHome,setCategory } = appSlice.actions
+export const { toggleSidebar,setHome,setCategory,setSeachSuggestion } = appSlice.actions
 
 export default appSlice.reducer
